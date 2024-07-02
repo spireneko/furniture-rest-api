07c0c57fb553cd6f61ced2bce242720921936715
spireneko <spireneko@gmail.com>
2024-07-02 22:40:24 +0400

    Get rid of useless struct (CreateRequest)

--- internal/service/service.go
+++ internal/service/service.go
@@ -20,37 +20,21 @@ func NewService(path string) Service {
 	}
 }
 
-type CreateRequest struct {
-	Name       string `json:"name"`
-	Fabricator string `json:"fabricator"`
-	Height     uint32 `json:"height"`
-	Width      uint32 `json:"width"`
-	Length     uint32 `json:"length"`
-}
-
 func (s *Service) Create(w http.ResponseWriter, r *http.Request) {
-	req := new(CreateRequest)
+	req := new(model.Furniture)
 	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
 		responseError(w, http.StatusBadRequest, err)
 		return
 	}
 	r.Body.Close()
 
-	newFurniture := model.Furniture{
-		Name:       req.Name,
-		Fabricator: req.Fabricator,
-		Height:     req.Height,
-		Width:      req.Width,
-		Length:     req.Length,
-	}
-
-	if err := repository.Create(&newFurniture, &s.JSONDB); err != nil {
+	if err := repository.Create(req, &s.JSONDB); err != nil {
 		responseError(w, http.StatusInternalServerError, err)
 		log.Printf("Error while adding data to db:%s", err)
 		return
 	}
 
-	response(w, http.StatusCreated, newFurniture)
+	response(w, http.StatusCreated, req)
 }
 
 func (s *Service) Get(w http.ResponseWriter, r *http.Request) {
@@ -87,26 +71,18 @@ func (s *Service) Update(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	req := new(CreateRequest)
+	req := new(model.Furniture)
 	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
 		responseError(w, http.StatusBadRequest, err)
 		return
 	}
 	r.Body.Close()
 
-	newFurniture := model.Furniture{
-		Name:       req.Name,
-		Fabricator: req.Fabricator,
-		Height:     req.Height,
-		Width:      req.Width,
-		Length:     req.Length,
-	}
-
-	if err := repository.Update(int64(id), &s.JSONDB, &newFurniture); err != nil {
+	if err := repository.Update(int64(id), &s.JSONDB, req); err != nil {
 		responseError(w, http.StatusNoContent, err)
 	}
 
-	response(w, http.StatusOK, newFurniture)
+	response(w, http.StatusOK, req)
 }
 
 func (s *Service) Patch(w http.ResponseWriter, r *http.Request) {
@@ -117,22 +93,14 @@ func (s *Service) Patch(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	req := new(CreateRequest)
+	req := new(model.Furniture)
 	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
 		responseError(w, http.StatusBadRequest, err)
 		return
 	}
 	r.Body.Close()
 
-	newFurniture := model.Furniture{
-		Name:       req.Name,
-		Fabricator: req.Fabricator,
-		Height:     req.Height,
-		Width:      req.Width,
-		Length:     req.Length,
-	}
-
-	if err := repository.Patch(int64(id), &s.JSONDB, &newFurniture); err != nil {
+	if err := repository.Patch(int64(id), &s.JSONDB, req); err != nil {
 		responseError(w, http.StatusNoContent, err)
 	}
 
